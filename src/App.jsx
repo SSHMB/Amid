75a1e5234031352cf954a24781816c7e2071509e
Kuplinov7788 <emirhan.7788.8899@gmail.com>
2026-05-02 12:11:53 +0500

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>safiya</h1>
-      <h1>Shamsiddin</h1>
-      <h1>Bilol</h1>
-      <h1>mirafzal</h1> 
-
-
-
-      <h1>emirhan jonka</h1>
+   
     </div>
   )
 }
