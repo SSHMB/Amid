68789c53b3e766a8ae7a7c6dda7ba24e31157ac0
bilolgulomqodirov <gbilol7779@gmail.com>
2026-05-05 11:22:38 +0500

    jdncjkas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from './Components/Header'
+import Banner from './Components/Banner'
+import Public from './Components/Public'
 
 const App = () => {
   return (
     <div>
       <Header />
-
+      <Banner />
+      <Public />
     </div>
   )
 }
